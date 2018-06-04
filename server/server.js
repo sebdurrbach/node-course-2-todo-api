60210b7e6892fbd4c71217da97e0faac6df4f60b
sdurrbach <sebastien.durrbach@gmail.com>
2018-06-04 22:15:49 +0200

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -16,8 +16,16 @@ app.post('/todos', (req, res) => {
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
-    res.status(400).send();
+    res.status(400).send(e);
   });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  });
+}, (e) => {
+  res.status(400).send(e);
 });
 
 app.listen(3000, () => {
