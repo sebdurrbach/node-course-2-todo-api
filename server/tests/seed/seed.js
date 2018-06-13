c02552c49b8c96341dbef68a62c66266cab57669
sdurrbach <sebastien.durrbach@gmail.com>
2018-06-13 16:41:53 +0200

    Add tests for POST/users & GET/users/me

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,49 @@
+const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+const users = [{
+  _id: userOneId,
+  email: 'seb@exemple.com',
+  password: 'userOnePass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+  }]
+}, {
+  _id: userTwoId,
+  email: 'slim@exemple.com',
+  password: 'userTwoPass'
+}];
+
+const todos = [{
+  _id: new ObjectID(),
+  text: 'First test todo'
+}, {
+  _id: new ObjectID(),
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 123
+}];
+
+const populateTodos = (done) => {
+  Todo.remove({}).then(() => {
+    Todo.insertMany(todos);
+  }).then(() => done());
+};
+
+// Insert many ne peut fonctionner avec le middleware
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    let userOne = new User(users[0]).save();
+    let userTwo = new User(users[1]).save();
+
+    return Promise.all([userOne, userTwo]); // Attend que toutes les promesses soient retournées (Array)
+  }).then(() => done());
+}
+
+module.exports = {todos, populateTodos, users, populateUsers};
